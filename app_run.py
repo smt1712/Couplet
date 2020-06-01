9ec2fba5632705a0efdac76e4578b3275a4c24e4
h2q <54574493+h2q@users.noreply.github.com>
2020-06-01 09:29:57 +0800

    对输入做限制

--- app_run.py
+++ app_run.py
@@ -1,12 +1,12 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 import tensorflow.compat.v1 as tf
-from model import Model
-from model import Config
+from model import Config,Model
 from flask import render_template
 from read_utils import TextConverter, batch_generator
 from flask_bootstrap import Bootstrap
 
+# from model_att_bi import Config,Model
 
 import os
 import numpy as np
@@ -26,9 +26,16 @@ model_dir = '/models/lstm_c'
 def hello():
   return render_template('test.html')
 
+def isAllChinese(s):
+ for c in s:
+  if not('\u4e00' <= c <= '\u9fa5'):
+   return False
+ return True
+
 @app.route('/cp', methods=['POST'])
 def cp():
     in_str=request.form.get('in_str');
+    str_in=in_str
     print("input_str="+in_str)
     model_path = os.path.join('models', Config.file_name)
     if os.path.exists(model_path) is False:
@@ -44,8 +51,8 @@ def cp():
     en_arr, arr_len = converter.text_en_to_arr(in_str)
     test_g = [np.array([en_arr, ]), np.array([arr_len, ])]
   #  output_ids = m.test(test_g, model_path, converter)
-    if len(in_str) == 0 or len(in_str) > 50:
-        output = u'您的输入太长了'
+    if len(in_str) == 0 or len(in_str)> 50  or isAllChinese(str_in)==False:
+        output = u'您的输入有误'
         print(len(in_str))
     else:
         output_ids =m.test(test_g, model_path, converter)
@@ -55,7 +62,7 @@ def cp():
        # in_str = converter.arr_to_text(in_str)
     print('上联：%s；下联：%s' % (in_str, output))
    #return jsonify({'output': output,'in_str': in_str})
-    return jsonify({"resultCode": 200, "error": "", "in_str" : in_str,"output" : output})
+    return jsonify({"resultCode": 200, "error": "", "in_str" : str_in,"output" : output})
    # return render_template('test.html', in_str=in_str,output=output)
 
 if __name__ == '__main__':
